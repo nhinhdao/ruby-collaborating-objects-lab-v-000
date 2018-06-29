2402da7a2d54d043f836dcb17552c5d8a6e0f619
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-29 15:19:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     def artist_name=(name)
         self.artist = Artist.find_or_create_by_name(name)
         artist.add_song(self)
-        binding.pry
+        # binding.pry
     end
 
     def self.new_by_filename(filename)
