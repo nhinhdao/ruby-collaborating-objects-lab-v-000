9aa7331f1aadf1687b3dc19fd1fb69d5a3a0b9ec
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-29 14:52:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
     end
 
     def self.new_by_filename(filename)
-        binding.pry
+        # binding.pry
         song = Song.new(filename.split(/\ - /)[1])
         song.artist = Artist.find_or_create_by_name(filename.split(/\ - /)[0])
         song
