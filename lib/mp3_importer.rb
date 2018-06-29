f2bdca27881cade0bf1ec6b54057e10a5d98eb19
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-29 14:35:16 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ class MP3Importer
 
     def files
 
-        @files = Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}/","")}
+        @files ||= Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}/","")}
     end
 
     def import
